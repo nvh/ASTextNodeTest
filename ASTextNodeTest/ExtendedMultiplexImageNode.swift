f1f6cbbe67b88d6ddb4c790104f01b2d0c0896a4
Niels van Hoorn <nvh@nvh.io>
2016-01-26 08:26:09 +0100

    Added extension on MultiplexImageNode

---
+++ ASTextNodeTest/ExtendedMultiplexImageNode.swift
@@ -0,0 +1,13 @@
+//
+//  ExtendedMultiplexImageNode.swift
+//  ASTextNodeTest
+//
+//  Created by Niels van Hoorn on 26/01/16.
+//  Copyright © 2016 Ruguo. All rights reserved.
+//
+
+import AsyncDisplayKit
+
+class ExtendedMultiplexImageNode: ASMultiplexImageNode {
+    
+}
